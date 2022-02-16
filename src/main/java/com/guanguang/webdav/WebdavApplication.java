eb469448eaf868df6ac842754ef50f4df55a9574
GuangGuang <google@liop.dev>
2022-02-16 09:24:38 +0000

    init

---
+++ src/main/java/com/guanguang/webdav/WebdavApplication.java
@@ -0,0 +1,13 @@
+package com.guanguang.webdav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebdavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebdavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/guanguang/webdav/WebdavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.guanguang.webdav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebdavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
